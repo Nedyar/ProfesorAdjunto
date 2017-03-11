5c0d4cbdbd906d8d5db22e4b680243930d05ba3d
yupipi93 <yupipi93@gmail.com>
2017-03-11 03:28:17 +0100

    crear menu activity

---
+++ app/src/main/java/sixpi/profesoradjunto/CrearCursoActivity.java
@@ -0,0 +1,13 @@
+package sixpi.profesoradjunto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CrearCursoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_curso);
+    }
+}
